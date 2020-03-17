030bec6c939d3339d2ddb11e888f8e20b35072bc
vstefanovic-grid <vstefanovic@griddynamics.com>
2020-03-18 00:17:25 +0100

    Finish js functionality and add new styles

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const LogicCtrl = (function(){
         const state = {
                 currPlayer: 'x',
                 gameActive: true,
-                grid: ['', '', '', '', '', '', '', ''],
+                grid: ['', '', '', '', '', '', '', '',''],
                 moves: 0,
+                winCombination: [],
         }
         const winningCombinations = [
                 [0, 1, 2],
@@ -26,7 +27,8 @@ const LogicCtrl = (function(){
                             continue;
                         }
                         if (a === b && b === c) {
-                            return true;
+                                state.winCombination = winCombination;
+                                return true;
                         }
                     }
                     return false;
@@ -35,12 +37,11 @@ const LogicCtrl = (function(){
         return {
                 handlePlay: function(cellIndex){
                         state.grid[cellIndex] = state.currPlayer;
+                        state.moves++;
                         if(isGameOver()) {
                                 state.gameActive = false;
                                 return;
                         }
-                        state.currPlayer = state.currPlayer === 'x'? 'o': 'x';
-                        state.moves++;
                 },
                 getGameActive: function(){
                         return state.gameActive;
@@ -48,70 +49,108 @@ const LogicCtrl = (function(){
                 reset: function() {
                         state.currPlayer = 'x';
                         state.gameActive = true;
-                        state.grid = ['', '', '', '', '', '', '', ''];
+                        state.grid = ['', '', '', '', '', '', '', '', ''];
                         state.moves = 0;
                 },
                 getWinnerInfo: function() {
                         const winnerInfo = {};
                         winnerInfo.winner = state.currPlayer;
-                        winnerInfo.moves = state.movers;
+                        winnerInfo.moves = state.moves;
                         return winnerInfo;
                 },
                 addWinner: function(name) {
-                        const winnerList = JSON.parse(localStorage.getItem('winners'));
+                        let winnerList = JSON.parse(localStorage.getItem('winners'));
                         const winner = {};
                         winner.name = name;
                         winner.symbol = state.currPlayer;
                         winner.moves = state.moves;
-
                         if(winnerList){
                                 winnerList = [...winnerList, winner];
                         }
                         else {
                                 winnerList = [winner];
                         }
-                        localStorage.setItem(JSON.stringify(winnerList));
+                        localStorage.setItem('winners',JSON.stringify(winnerList));
+                },
+                
+                nextPlayer: function() {
+                        state.currPlayer = state.currPlayer === 'x'? 'o': 'x';
+                },
+                isCellEmpty(cellIndex) {
+                        return state.grid[cellIndex] === '';
+                },
+                getWinCombination() {
+                        return state.winCombination;
                 }
         };
 })();
 
 const UICtrl = (function(){
         return {
-                getCellIndex: function(e){
-                        console.log(e);
-                        Array.from(e.target.parentNode.children).indexOf(e.target);
+                getCellIndex: function(target){
+                        return Array.from(target.parentNode.children).indexOf(target);
                 },
                 showWinnerInfo: function(winnerInfo){
                         const winnerHeader = document.querySelector('.game__winner');
                         winnerHeader.classList.toggle('game__winner--show');
-                        winnerHeader.textContent = `Winner ${winnerInfo.winner}, Moves: ${winnerInfo.moves}`
+                        winnerHeader.textContent = `Winner: ${winnerInfo.winner}, Moves: ${winnerInfo.moves}`
                 },
                 showForm: function() {
                         const frm = document.querySelector('.game__form');
                         frm.classList.toggle('game__form--show')
                 },
                 getName: function(e) {
-                        const formData = new frmData(e.target);
-                        return formData.get('name');
+                        const formData = new FormData(e.target);
+                        return formData.get('winnerName');
                 },
                 updateWinnerList() {
                         const winnerList = JSON.parse(localStorage.getItem('winners'));
-                        winnerList.forEach(winner => {
-                                document.querySelector('.winner-list').insertAdjacentHTML('beforeend', 
-                                `<li class="winner-list__item">Winner: ${winner.name}, &nbsp;Symbol: ${winner.symbol}, Moves ${winner.moves}</li>`);
-                        })
+                        if(winnerList){
+                                winnerList.forEach(winner => {
+                                        document.querySelector('.winner-list').insertAdjacentHTML('beforeend', 
+                                        `<li class="winner-list__item">Winner: ${winner.name}, &nbsp;Symbol: ${winner.symbol}, Moves ${winner.moves}</li>`);
+                                })
+                        }
                 },
                 addWinner(name, winnerInfo) {
                         document.querySelector('.winner-list').insertAdjacentHTML('beforeend', 
                         `<li class="winner-list__item">Winner: ${name}, &nbsp;Symbol: ${winnerInfo.winner}, Moves ${winnerInfo.moves}</li>`);
+                        document.querySelector('input').disabled = true;
+                        document.querySelector('.btn--submit').classList.add('btn--disabled');
                 },
-                updateCell(e, winnerInfo) {
+                updateCell(target, winnerInfo) {
                         if(winnerInfo.winner === 'o'){
-                                e.target.children[0].classList.add('circle');
+                                target.children[0].classList.add('circle');
                         }
                         else{
-                                e.target.children[0].classList.add('cross');
+                                target.children[0].classList.add('cross');
                         }
+                },
+                reset(){
+                        const items = document.getElementsByClassName('cell-item');
+                        Array.from(items).forEach(item => {
+                                item.classList.remove('circle');
+                                item.classList.remove('cross');
+                                item.classList.remove('cell-item--dark');
+                                item.classList.remove('cell-item--light');
+
+                        });
+                        document.querySelector('.game__winner').classList.remove('game__winner--show');
+                        document.querySelector('.game__form').classList.remove('game__form--show');
+                        document.querySelector('input').disabled = false;
+                        document.querySelector('.btn--submit').classList.remove('btn--disabled');
+                        
+                },
+                highlightWinnerSymbol: function(winCombination){
+                        const cells = document.getElementsByClassName('cell-item');
+                        Array.from(cells).forEach((cell, i) => {
+                                if(winCombination.indexOf(i) !== -1){
+                                        cell.classList.add('cell-item--light');
+                                }
+                                else{
+                                        cell.classList.add('cell-item--dark');
+                                }
+                        })
                 },
         
         }
@@ -119,23 +158,39 @@ const UICtrl = (function(){
 
 const ctrl = (function(LogicCtrl, UICtrl){
         function handleMove(e){
+                let target;
+                if(e.target.classList.contains('cell-item')){
+                        target = e.target.parentNode;
+                }
+                else {
+                        target = e.target;
+                }
                 if(LogicCtrl.getGameActive()){
-                        cellIndex = UICtrl.getCellIndex(e);
-                        LogicCtrl.handlePlay(cellIndex);
-                        UICtrl.updateCell(e, LogicCtrl.getWinnerInfo())
-                        if(!LogicCtrl.getGameActive()){
-                                const winnerInfo = LogicCtrl.getWinnerInfo();
-                                UICtrl.showWinnerInfo(winnerInfo);
-                                UICtrl.showForm();
-                        }
+                        const cellIndex = UICtrl.getCellIndex(target);
+                        if(LogicCtrl.isCellEmpty(cellIndex)){
+                                LogicCtrl.handlePlay(cellIndex);
+                                UICtrl.updateCell(target, LogicCtrl.getWinnerInfo());
+                                if(!LogicCtrl.getGameActive()){
+                                        const winnerInfo = LogicCtrl.getWinnerInfo();
+                                        UICtrl.highlightWinnerSymbol(LogicCtrl.getWinCombination());
+                                        UICtrl.showWinnerInfo(winnerInfo);
+                                        UICtrl.showForm();
+                                }
+                                LogicCtrl.nextPlayer();
+                        }                      
+                        
                 }       
         }
         function handleNameInput(e) {
+                console.log(e);
                 e.preventDefault();
                 const name = UICtrl.getName(e);
                 const winnerInfo = LogicCtrl.getWinnerInfo();
                 LogicCtrl.addWinner(name);
                 UICtrl.addWinner(name, winnerInfo);
+                LogicCtrl.reset();
+        }
+        function restart(){
                 LogicCtrl.reset();
                 UICtrl.reset();
         }
@@ -144,10 +199,12 @@ const ctrl = (function(LogicCtrl, UICtrl){
                         el.addEventListener('click', handleMove);
                 })
                 document.querySelector('.game__form').addEventListener('submit', handleNameInput);
+                document.querySelector('.btn--reset').addEventListener('click', restart);
         }
         return {
                 init: function() {
                         setupEventListeners();
+                        UICtrl.updateWinnerList();
                 }
         }
 })(LogicCtrl, UICtrl);
